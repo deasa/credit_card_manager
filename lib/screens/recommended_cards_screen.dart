c91d1168b94ed4912f2b97ee9331cf4ea3ca1e0a
Brendan Ashton <bgeorgeashton@gmail.com>
2023-03-23 21:07:13 -0600

    recommended_cards_screen.dart

---
+++ lib/screens/recommended_cards_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import '../models/credit_card.dart';
+import '../services/card_service.dart';
+
+class RecommendedCardsScreen extends StatefulWidget {
+  @override
+  _RecommendedCardsScreenState createState() => _RecommendedCardsScreenState();
+}
+
+class _RecommendedCardsScreenState extends State<RecommendedCardsScreen> {
+  CardService _cardService = CardService();
+  List<CreditCard> _recommendedCards;
+
+  @override
+  void initState() {
+    super.initState();
+    _recommendedCards = _cardService.getRecommendedCards();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Recommended Cards'),
+      ),
+      body: ListView.builder(
+        itemCount: _recommendedCards.length,
+        itemBuilder: (context, index) {
+          CreditCard card = _recommendedCards[index];
+          return Card(
+            child: ListTile(
+              title: Text(card.name),
+              subtitle: Text(card.issuer),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
